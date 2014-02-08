5dbee666f6eef80491944fc78fd17c04cbd4bc33
geneviethebeezy <geneva.capos.862@my.csun.edu>
2014-02-08 15:02:53 -0800

    3rd commit - adding apache class
    setting up http request

--- In_Plane_App/src/com/csungreen/In_Plane_App/Networking.java
+++ In_Plane_App/src/com/csungreen/In_Plane_App/Networking.java
@@ -2,16 +2,49 @@ package com.csungreen.In_Plane_App;
 
 import android.app.Activity;
 import android.os.Bundle;
-import org.apache.*;
+import android.widget.TextView;
+import org.apache.http.ProtocolVersion;
+import org.apache.http.RequestLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 
 public class Networking extends Activity
 {
+    TextView display;
+    String video;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+    }
+    public RequestLine getRequestLine() {
+        RequestLine req = new RequestLine() {
+            @Override
+            public String getMethod() {
+                return null;
+            }
+
+            @Override
+            public ProtocolVersion getProtocolVersion() {
+                return null;
+            }
+
+            @Override
+            public String getUri() {
+                HttpClient ourServer = new DefaultHttpClient();
+                HttpPost postServer = new HttpPost("http://172.31.176.125");
+                return null;
+            }
+        };
+
+
+        display.setText("Here's your video: " + video);
+        return req;
 
     }
 }
