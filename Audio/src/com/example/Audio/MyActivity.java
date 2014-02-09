0606ff195a160113cf54e3a76d89a7642bcedc6b
Ian Graham <ian.graham.534@my.csun.edu>
2014-02-08 18:48:05 -0800

    Media Video and Audio
    Must merge into another App

---
+++ Audio/src/com/example/Audio/MyActivity.java
@@ -0,0 +1,47 @@
+package com.example.Audio;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.app.Activity;
+import android.content.Intent;
+
+public class MyActivity extends Activity {
+
+    Button video;
+    Button music;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Get the layout from video_main.xml
+        setContentView(R.layout.main);
+
+        // Locate the button in activity_main.xml
+       video = (Button) findViewById(R.id.MyVideoButton);
+       music = (Button) findViewById(R.id.MyMusicButton);
+        // Capture button clicks
+        video.setOnClickListener(new OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MyActivity.this,
+                        VideoViewActivity.class);
+                myIntent.putExtra("streamType", "video");
+                startActivity(myIntent);
+            }
+        });
+        music.setOnClickListener(new OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MyActivity.this,
+                        VideoViewActivity.class);
+                myIntent.putExtra("streamType", "music");
+                startActivity(myIntent);
+            }
+        });
+    }
+
+}
---
+++ Audio/src/com/example/Audio/VideoViewActivity.java
@@ -0,0 +1,78 @@
+package com.example.Audio;
+
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.VideoView;
+import android.content.Intent;
+
+public class VideoViewActivity extends Activity {
+
+    // Declare variables
+    ProgressDialog pDialog;
+    VideoView videoview;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Get the layout from video_main.xml
+        setContentView(R.layout.videoview_main);
+        // Find your VideoView in your video_main.xml layout
+        videoview = (VideoView) findViewById(R.id.VideoView);
+        // Execute StreamVideo AsyncTask
+
+        // Insert your Video/Music URL
+        Intent myIntent = getIntent();
+        String streamType = myIntent.getStringExtra("streamType");
+        String URL = "";
+        if(streamType.equals("music"))
+            URL = "http://172.31.176.125/testSong.mp3";
+        else
+            URL = "http://172.31.176.125/test.mp4";
+
+        // Create a progressbar
+        pDialog = new ProgressDialog(VideoViewActivity.this);
+        // Set progressbar title
+        pDialog.setTitle("Your " + streamType + " will be ready soon.");
+        // Set progressbar message
+        pDialog.setMessage("Buffering...");
+        pDialog.setIndeterminate(false);
+        pDialog.setCancelable(false);
+        // Show progressbar
+        pDialog.show();
+
+        try {
+            // Start the MediaController
+            MediaController mediacontroller = new MediaController(
+                    VideoViewActivity.this);
+            mediacontroller.setAnchorView(videoview);
+            // Get the URL from String URL
+            Uri video = Uri.parse(URL);
+            videoview.setMediaController(mediacontroller);
+            videoview.setVideoURI(video);
+
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+
+        videoview.requestFocus();
+        videoview.setOnPreparedListener(new OnPreparedListener() {
+            // Close the progress bar and play the video
+            public void onPrepared(MediaPlayer mp) {
+                pDialog.dismiss();
+                videoview.start();
+            }
+        });
+
+    }
+
+}
